1ea6b7e2096ec183e25ffa71739809d8364fdd8d
komal srivastava <86881218+komal9ks@users.noreply.github.com>
2023-10-31 23:31:01 +0530

    Add files via upload

---
+++ factorial.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    int factorial = 1;
+
+    while (n > 0) {
+        factorial *= n;
+        n--;
+    }
+
+    cout << "Factorial of 5 is: " << factorial << endl;
+
+    return 0;
+}
