7d18c41a877806a7774daa2e63e7aac341c996d7
komal srivastava <86881218+komal9ks@users.noreply.github.com>
2023-11-02 21:16:09 +0530

    Add files via upload

---
+++ datatypes.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string type;
+ if (type == "Integer") {
+    return 4;
+  } else if (type == "Long") {
+    return 8;
+  } else if (type == "Float"){
+    return 4;
+  } else if (type == "Double") {
+    return 8;
+  } else if (type == "Character"){
+    return 1;
+}
+}
